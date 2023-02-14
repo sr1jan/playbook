ec9f7d30de596c1460c744bf0ccb89e53ae8463c
∞ infi <106063205+InfiniteGwei@users.noreply.github.com>
2023-02-14 16:20:38 +0530

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -3,5 +3,3 @@
 set -ev
 
 Rscript -e "bookdown::render_book('index.rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.rmd', 'bookdown::epub_book')"
