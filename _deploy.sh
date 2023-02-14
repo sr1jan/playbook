69396e4e8ea5502c25c6b30011c1f605085e8866
∞ infi <106063205+InfiniteGwei@users.noreply.github.com>
2023-02-14 16:34:36 +0530

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-[ -z "${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}" ] && exit 0
-[ "${gh-pages}" != "master" ] && exit 0
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
 git config --global user.email "infinitegwei@skiff.com"
 git config --global user.name "infi"
 
-git clone -b gh-pages https://${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}@github.com/${gh-pages}.git book-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
